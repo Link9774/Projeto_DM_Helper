ca8868a9f5ef6b761fbacd63cc2216b982070319
Pedro Henrique de Oliveira <111504681+Link9774@users.noreply.github.com>
2025-10-30 12:51:43 -0300

    Adição dos pacotes

---
+++ Projeto_RPGDM Helper/Projeto_RPGDM Helper/personagens/Monster.java
@@ -0,0 +1,18 @@
+package personagens;
+
+public class Monster extends Personagem{
+    private String tipo;
+    private int dano;
+    private String tipoDano;
+
+    public Monster(String nome,String tipo, String tipoDano ,int nivel, int vidaMaxima, int dano){
+        super(nome, nivel, vidaMaxima);
+        this.tipo = tipo;
+        this.dano = dano;
+        this.tipoDano = tipoDano;
+    }
+    @Override
+    public void mostrarInfo(){
+         System.out.println("Nome: " + nome + " | Tipo do monstro: " +tipo+ " | Dano: " + dano + " | Tipo de Dano: " + tipoDano + " | Nivel: " + nivel+ " | Vida " +vida+ "/" +vidaMaxima);
+    }
+}
